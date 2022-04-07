cb21781dd7b860e6741cdfce234ee6d7a89c84c6
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2022-04-07 13:00:22 +0300

    Fix test

--- src/main/kotlin/com/itmo/microservices/demo/bombardier/stages/OrderChangeItemsAfterFinalizationStage.kt
+++ src/main/kotlin/com/itmo/microservices/demo/bombardier/stages/OrderChangeItemsAfterFinalizationStage.kt
@@ -10,7 +10,9 @@ import com.itmo.microservices.demo.bombardier.logging.OrderChangeItemsAfterFinal
 import com.itmo.microservices.demo.bombardier.utils.ConditionAwaiter
 import com.itmo.microservices.demo.common.logging.EventLoggerWrapper
 import org.springframework.stereotype.Component
+import java.util.UUID
 import java.util.concurrent.TimeUnit
+import kotlin.math.min
 import kotlin.random.Random
 
 @Component
@@ -32,20 +34,32 @@ class OrderChangeItemsAfterFinalizationStage : TestStage {
         eventLogger.info(I_START_CHANGING_ITEMS, testCtx().orderId)
 
         val items = externalServiceApi.getAvailableItems(testCtx().userId!!)
+        val theOrderBefore = externalServiceApi.getOrder(testCtx().userId!!, testCtx().orderId!!)
         repeat(Random.nextInt(1, 5)) {
             val itemToAdd = items.random()
+            if (itemToAdd.amount < 1) return@repeat
 
-            val amount = Random.nextInt(1, 13)
+            val amount = Random.nextInt(1, min(itemToAdd.amount, 13))
             externalServiceApi.putItemToOrder(testCtx().userId!!, testCtx().orderId!!, itemToAdd.id, amount)
 
             ConditionAwaiter.awaitAtMost(3, TimeUnit.SECONDS)
                 .condition {
                     val theOrder = externalServiceApi.getOrder(testCtx().userId!!, testCtx().orderId!!)
-                    theOrder.itemsMap.any { it.key.id == itemToAdd.id && it.value == amount }
+
+                    theOrder.itemsMap.any {
+                        it.key.id == itemToAdd.id &&
+                                it.value == amount +
+                                (theOrderBefore.itemsMap.firstNotNullOfOrNull {
+                                        it2 -> if (it2.key.id == it.key.id) it2.value else null
+                                } ?: 0)
+                    }
                             && theOrder.status == OrderStatus.OrderCollecting
                 }
                 .onFailure {
                     eventLogger.error(E_ORDER_CHANGE_AFTER_FINALIZATION_FAILED, itemToAdd.id, amount, testCtx().orderId)
+                    val theOrder = externalServiceApi.getOrder(testCtx().userId!!, testCtx().orderId!!)
+                    println("${itemToAdd.id}: $amount ${theOrder.itemsMap.filter { it.key.id == itemToAdd.id }}")
+                    println("${theOrderBefore.itemsMap.firstNotNullOfOrNull {  }}")
                     if (it != null) {
                         throw it
                     }
